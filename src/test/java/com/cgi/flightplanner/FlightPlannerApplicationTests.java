83a6a9f02dc275b4422353874a35b5a82edcbded
Kaur Kivilaan <kaurkivi@gmail.com>
2025-03-24 22:26:46 +0200

    Initial project commit

---
+++ src/test/java/com/cgi/flightplanner/FlightPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi.flightplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
