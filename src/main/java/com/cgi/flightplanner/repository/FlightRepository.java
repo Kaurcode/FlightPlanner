fb984c39edf0b3e369ab7a264bfd60c356eec454
Kaur Kivilaan <kaurkivi@gmail.com>
2025-03-26 22:50:04 +0200

    Added Spring Data JPA repository methods to FlightRepository for finding flights by origin airport and departure and arrival time Spring Data JPA link for JPA Query Methods: https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html

--- src/main/java/com/cgi/flightplanner/repository/FlightRepository.java
+++ src/main/java/com/cgi/flightplanner/repository/FlightRepository.java
@@ -1,9 +1,19 @@
 package com.cgi.flightplanner.repository;
 
+import com.cgi.flightplanner.entities.Airport;
 import com.cgi.flightplanner.entities.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.List;
+
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+    // https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+    List<Flight> findByOriginAndDepartureTimeAfter(Airport origin, Instant departureTime);
+
+    List<Flight> findByOriginAndDepartureTimeAfterAndArrivalTimeBefore(
+            Airport origin, Instant departureTime, Instant arrivalTime
+    );
 }
